63abceb9aa2b6d05af31565df57879eae8cce9b7
Corama <razod@tele.com>
2020-03-27 15:25:35 +0300

    Include middleware functions for body parse and basic routes

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ const path = require('path');
 const app = express();
 
 // Set static files
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+
+/**
+ * Routes
+ */
+// Index Routes
+const indexPage = require('./routes/index');
+app.use('/', indexPage);
+
+// Register Routes
+const registerPage = require('./routes/register');
+app.use('/', registerPage);
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server started on port: " + PORT));
 
