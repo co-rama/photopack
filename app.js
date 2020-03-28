66a2142b801613e57298e4afe8c84bc15b47e0d9
Corama Yunus <coramayunus@gmail.com>
2020-03-28 03:12:23 +0300

    Add dotenv and mongoose db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
@@ -12,6 +14,15 @@ app.set('view engine', 'ejs');
 // Middlewares
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// DatabaseConnection
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, (err) =>{
+    if(err){
+        console.error(err);
+    }else{
+        console.log('Connected to the database server')
+    }
+})
 
 
 /**
@@ -27,6 +38,4 @@ app.use('/', registerPage);
 
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log("Server started on port: " + PORT));
-
